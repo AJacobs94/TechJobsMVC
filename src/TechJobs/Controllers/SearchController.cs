3793dd87dea761cf2ec1451162fed3c3a575a4b2
AJacobs94 <56659280+AJacobs94@users.noreply.github.com>
2020-03-11 00:30:20 -0500

    Finished Assignment 2

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,8 +13,34 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
 
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> results = JobData.FindByValue(searchTerm = "");
+
+                ViewBag.title = "All " + ListController.columnChoices[searchType] + " Values";
+                ViewBag.title = "All Jobs Results";
+                ViewBag.jobs = results;
+                ViewBag.columns = ListController.columnChoices;
+
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<string, string>> searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "All " + ListController.columnChoices[searchType] + " Values";
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = searchResults;
+                return View("Index");
+            }
+
+            // TODO #1 - Create a Results action method to process 
+            // search request and display results
+
+        }
     }
+
 }
+
